a6a05d329416e56ebd68d715b5747c3cc7b8a83e
andrespallareslopez <andres_pallares@hotmail.com>
2024-05-10 20:18:57 +0200

    Actualizar codigo

---
+++ SqlTemplate01.sql
@@ -0,0 +1,23 @@
+﻿		  
+		   
+			IdActivo - IDX_IdActivo_asc
+		
+	
+			IdActivo - pk_EstrategiaAACC
+		
+			  
+		   
+			FcInicio - pk_EstrategiaAACC
+		
+			  
+		   		  
+		   
+			IdEstrategia - IDX_IdEstrategia_asc
+		
+	
+			IdEstrategia - IDX_IdEstrategia_desc
+		
+			  
+		   		  
+		   		  
+		   
