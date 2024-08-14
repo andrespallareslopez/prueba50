9461fd1876ce650826ab1c86255a0c25d459aee3
andrespallareslopez <andres_pallares@hotmail.com>
2024-08-14 12:22:19 +0200

    actualizar codigo

--- SqlTemplate01.sql
+++ SqlTemplate01.sql
@@ -1,23 +1,24 @@
-﻿		  
-		   
-			IdActivo - IDX_IdActivo_asc
-		
-	
-			IdActivo - pk_EstrategiaAACC
-		
-			  
-		   
-			FcInicio - pk_EstrategiaAACC
-		
-			  
-		   		  
-		   
-			IdEstrategia - IDX_IdEstrategia_asc
-		
-	
-			IdEstrategia - IDX_IdEstrategia_desc
-		
-			  
-		   		  
-		   		  
-		   
+﻿
+   
+    Id - decimal - decimal - 9
+   
+    TipoActivo - varchar - varchar - 4
+   
+    IdModeloGestion - int - int - 4
+   
+    IdServicerComercial - int - int - 4
+   
+    IdPerimetroOper - int - int - 4
+   
+    IdTerritorial - int - int - 4
+   
+    FcInicio - datetime - datetime - 8
+   
+    FcIniProceso - datetime - datetime - 8
+   
+    FcFinProceso - datetime - datetime - 8
+   
+    IdValidacion - nvarchar - nvarchar - 255
+   
+    UserAudit - nvarchar - nvarchar - 255
+   
